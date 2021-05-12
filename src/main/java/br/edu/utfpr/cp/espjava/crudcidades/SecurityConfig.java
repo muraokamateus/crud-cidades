a67bcd61d245f56c558e8824cc40421751e21198
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-05-12 13:08:52 -0300

    - Added code.

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
@@ -1,15 +1,16 @@
 package br.edu.utfpr.cp.espjava.crudcidades;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.edu.utfpr.cp.espjava.crudcidades.usuario.Usuario;
 
 @EnableWebSecurity
 @Configuration
@@ -38,8 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void printSenhas() {
-        System.out.println(this.cifrador().encode("test123"));
+    @EventListener(InteractiveAuthenticationSuccessEvent.class)
+    public void printUsuarioAtual(InteractiveAuthenticationSuccessEvent event) {
+
+        var usuario = (Usuario) event.getAuthentication().getPrincipal();
+
+        System.out.println(usuario.getNome());
     }
 }
